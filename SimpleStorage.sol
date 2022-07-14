f105a60e1b5baebb8fb8e166e20cbbad96134685
dontech09 <donnjondanmakal@gmail.com>
2022-07-14 19:40:03 +0530

    types

---
+++ SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:MIT
+pragma solidity 0.8.8; //Specify the version of Solidity .Important to mention the Licence used 
+
+contract SimpleStorage{
+    //contract keyword to specify a contract
+
+    //Types
+    ////boolean, uint, int, address, bytes
+    ///uint you cna specify how many bits default value uint256
+    uint8 hasFavNumber=128;
+    string name="India";   
+    int256 favInt=-4;
+    address myAddress=0xe277F9Ae64551ff1C1aD2cCd707F4bDAFCAFD36C;
+}
