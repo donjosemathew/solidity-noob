557b9b1ca2338d66f9c53607080bbec3ae85ecd7
dontech09 <donnjondanmakal@gmail.com>
2022-07-28 11:29:17 +0530

    Fundme

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,40 +1,85 @@
-//SPDX-License-Identifier:MIT
-//ChainLink is technology used to
-// access outside data from a centralised system
-//Data feeds
-pragma solidity 0.8.8;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
-contract FundMe{
-    uint256 public minimumUSDollar=50 * 1e18;
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-    mapping(address => uint256) public addressToAmountfunds;
-    function fund( ) public payable{
-        //Want able to send minimu amount
-        //1.how do we send ETH
-        //payable mark function as funding 2.45
-        require(getConversionRate(msg.value)>=minimumUSDollar,"Didn't have enough to send");//1e18==1*10**18 ==1wei
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        addressToAmountfunds[msg.sender]=msg.value;
-        //reverting?if don't have enough balance the the gas spend for 
-        //prior operations will be returned
+    }
+    
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+    
+    modifier onlyOwner {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+    
+    function withdraw() payable onlyOwner public {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
     }
 
-    function getPrice() public view returns(uint256){
-        //ABI
-        //Addresss 
-        //to interact with outside data 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-        AggregatorV3Interface pricefeed=AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 price,,,)=pricefeed.latestRoundData();
-        //1ETH IN TERMS OF USD
-        //
-        return uint256(price*1e10);//1**10
-
+    receive() external payable {
+        fund();
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice=getPrice();
-        uint256 ethAmountInUsd=(ethPrice*ethAmount)/1e18;
-        return ethAmountInUsd;
-    }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
